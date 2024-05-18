8f9eb31a725da611d0e9b336c21cd9dd90110500
terrrhe <46038242+terrrhe@users.noreply.github.com>
2024-05-18 15:56:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def generate_image():
 def generate_result_image():
     q = request.form.get("q")
     output = replicate.run("stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
-      input = {"prompt":q}
+      input = {"prompt":q})
     
     return render_template(
          "generate_image_result.html", r = output)
